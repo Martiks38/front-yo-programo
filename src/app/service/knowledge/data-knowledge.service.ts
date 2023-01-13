0f1271c7bb716140c1253c7409e4540b7f37b9a7
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-01-13 12:59:45 -0300

    Cambiar url

--- src/app/service/knowledge/data-knowledge.service.ts
+++ src/app/service/knowledge/data-knowledge.service.ts
@@ -10,7 +10,7 @@ const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   providedIn: 'root',
 })
 export class DataKnowledgeService {
-  private apiUrl = 'http://localhost:8080/api/v1/';
+  private apiUrl = 'https://back-yo-programo-production.up.railway.app/';
 
   http = inject(HttpClient);
 
--- src/app/service/user/dataUser.service.ts
+++ src/app/service/user/dataUser.service.ts
@@ -8,7 +8,7 @@ const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   providedIn: 'root',
 })
 export class DataUserService {
-  private apiUrl = 'http://localhost:8080/api/v1/';
+  private apiUrl = 'https://back-yo-programo-production.up.railway.app/';
 
   http = inject(HttpClient);
 
